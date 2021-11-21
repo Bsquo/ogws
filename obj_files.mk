f049baf4c0d19107e76cccc167c493f2c9538e89
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2021-11-21 17:15:58 -0500

    Split `extras.s`

--- obj_files.mk
+++ obj_files.mk
@@ -317,6 +317,7 @@ STL_O_FILES := \
 	build/asm/STL/abort_exit_ppc_eabi.o \
 	build/asm/STL/math_sun.o \
 	build/asm/STL/math_float.o \
+	build/asm/STL/extras.o \
 	build/asm/STL/float.o \
 
 GROUP_1_O_FILES := \
